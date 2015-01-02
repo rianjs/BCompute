8351ff0f719557cd22e9eb400fb9b042ed8287a5
Rian Stockbower <rstockbower@gmail.com>
2015-01-01 20:54:34 -0500

    Working on mendellian inheritance

---
+++ BCompute.UnitTests/InheritanceUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BCompute.UnitTests
+{
+    [TestFixture]
+    public class InheritanceUnitTests
+    {
+        public double MendellianInheritanceTests(int homozygousDominant, int heterozygousDominant, int homozygousRecessive)
+        {
+            
+        }
+
+        public IEnumerable<ITestCaseData> MendellianInheritance_TestCases()
+        {
+            yield return new TestCaseData(2, 2, 2).Returns(0.78333);
+        }
+    }
+}
