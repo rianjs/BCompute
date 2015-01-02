c6038043549c91c42017fb6a80cafeb5434dac7b
Rian Stockbower <rstockbower@gmail.com>
2015-01-02 13:49:56 -0500

    Phenotype -> Genotype

--- BCompute.UnitTests/InheritanceUnitTests.cs
+++ BCompute.UnitTests/InheritanceUnitTests.cs
@@ -6,14 +6,35 @@ namespace BCompute.UnitTests
     [TestFixture]
     public class InheritanceUnitTests
     {
-        public double MendellianInheritanceTests(int homozygousDominant, int heterozygousDominant, int homozygousRecessive)
+        [Test, TestCaseSource("ParentalSetProbability_TestCases")]
+        public double ParentalSetProbability(uint homoDominantPopulation, uint heteroPopulation, uint homoRecessivePopulation, Genotype genotype)
         {
-            
+            return new PhenotypePopulation(homoDominantPopulation, heteroPopulation, homoRecessivePopulation).GetParentalSetProbability(genotype);
         }
 
-        public IEnumerable<ITestCaseData> MendellianInheritance_TestCases()
+        public IEnumerable<ITestCaseData> ParentalSetProbability_TestCases()
         {
-            yield return new TestCaseData(2, 2, 2).Returns(0.78333);
+            const double smallUniformPopulationParentalProbability = (2.0d / 6.0d) * (1.0d / 5.0d);
+            const uint small = 2;
+
+            yield return new TestCaseData(small, small, small, Genotype.HomozygousDominant).Returns(smallUniformPopulationParentalProbability)
+                .SetName("HomoDominant: 2x2x2 test cases all return the same number");
+            yield return new TestCaseData(small, small, small, Genotype.Heterozygous).Returns(smallUniformPopulationParentalProbability)
+                .SetName("Hetero: 2x2x2 test cases all return the same number");
+            yield return new TestCaseData(small, small, small, Genotype.HomozygousRecessive).Returns(smallUniformPopulationParentalProbability)
+                .SetName("HomoRecessive: 2x2x2 test cases all return the same number");
+        }
+
+        [Test, TestCaseSource("ChildAlleleManifestationProbability_TestCases")]
+        public double ChildAllelManifestationProbabilityTests(uint homoDominantPopulation, uint heteroPopulation, uint homoRecessivePopulation, Genotype genotype)
+        {
+            return new PhenotypePopulation(homoDominantPopulation, heteroPopulation, homoRecessivePopulation).GetChildAlleleProbability(genotype);
+        }
+
+        public IEnumerable<ITestCaseData> ChildAlleleManifestationProbability_TestCases()
+        {
+            const uint small = 2;
+            yield return new TestCaseData(small, small, small).Returns(0.78333);
         }
     }
 }
