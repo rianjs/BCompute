1db8ab2e0162e690cc178a29c54fbd4455acbd44
Rian Stockbower <rstockbower@gmail.com>
2015-01-04 17:13:30 -0500

    Added RNA to amino codon map

--- BCompute/Maps.cs
+++ BCompute/Maps.cs
@@ -5,6 +5,7 @@ namespace BCompute
 {
     internal sealed class Maps
     {
+        public const char StopTranslation = '*';
         private static ImmutableDictionary<char, char> _dnaComplements;
         public static ImmutableDictionary<char, char> DnaComplements
         {
@@ -42,6 +43,87 @@ namespace BCompute
                     _rnaComplements = rnaComplements.ToImmutableDictionary();
                 }
                 return _rnaComplements;
+            }
+        }
+
+        private static ImmutableDictionary<string, char> _codonToAminoAcid;
+
+        public static ImmutableDictionary<string, char> CodonToAmino
+        {
+            get
+            {
+                if (_codonToAminoAcid == null || _codonToAminoAcid.Count == 0)
+                {
+                    var codons = new Dictionary<string, char>
+                    {
+                        {"UUU", 'F'},
+                        {"CUU", 'L'},
+                        {"AUU", 'I'},
+                        {"GUU", 'V'},
+                        {"UUC", 'F'},
+                        {"CUC", 'L'},
+                        {"AUC", 'I'},
+                        {"GUC", 'V'},
+                        {"UUA", 'L'},
+                        {"CUA", 'L'},
+                        {"AUA", 'I'},
+                        {"GUA", 'V'},
+                        {"UUG", 'L'},
+                        {"CUG", 'L'},
+                        {"AUG", 'M'},
+                        {"GUG", 'V'},
+                        {"UCU", 'S'},
+                        {"CCU", 'P'},
+                        {"ACU", 'T'},
+                        {"GCU", 'A'},
+                        {"UCC", 'S'},
+                        {"CCC", 'P'},
+                        {"ACC", 'T'},
+                        {"GCC", 'A'},
+                        {"UCA", 'S'},
+                        {"CCA", 'P'},
+                        {"ACA", 'T'},
+                        {"GCA", 'A'},
+                        {"UCG", 'S'},
+                        {"CCG", 'P'},
+                        {"ACG", 'T'},
+                        {"GCG", 'A'},
+                        {"UAU", 'Y'},
+                        {"CAU", 'H'},
+                        {"AAU", 'N'},
+                        {"GAU", 'D'},
+                        {"UAC", 'Y'},
+                        {"CAC", 'H'},
+                        {"AAC", 'N'},
+                        {"GAC", 'D'},
+                        {"UAA", StopTranslation},
+                        {"CAA", 'Q'},
+                        {"AAA", 'K'},
+                        {"GAA", 'E'},
+                        {"UAG", StopTranslation},
+                        {"CAG", 'Q'},
+                        {"AAG", 'K'},
+                        {"GAG", 'E'},
+                        {"UGU", 'C'},
+                        {"CGU", 'R'},
+                        {"AGU", 'S'},
+                        {"GGU", 'G'},
+                        {"UGC", 'C'},
+                        {"CGC", 'R'},
+                        {"AGC", 'S'},
+                        {"GGC", 'G'},
+                        {"UGA", StopTranslation},
+                        {"CGA", 'R'},
+                        {"AGA", 'R'},
+                        {"GGA", 'G'},
+                        {"UGG", 'W'},
+                        {"CGG", 'R'},
+                        {"AGG", 'R'},
+                        {"GGG", 'G'},
+                    };
+                    _codonToAminoAcid = codons.ToImmutableDictionary();
+                }
+                return _codonToAminoAcid;
             }
         }
     }
