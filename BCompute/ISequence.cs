847d053db94b77ef648262a87c7cd6d0ac143561
Rian Stockbower <rstockbower@gmail.com>
2015-01-05 10:38:01 -0500

    Initial work on ISequence improvements

--- BCompute/ISequence.cs
+++ BCompute/ISequence.cs
@@ -4,8 +4,34 @@ namespace BCompute
 {
     public interface ISequence
     {
+        /// <summary>
+        /// Returns the sequence string
+        /// </summary>
         string Sequence { get; }
+
+        /// <summary>
+        /// Returns the number of gaps that are present in the sequence
+        /// </summary>
+        int GapCount { get; }
+
+        /// <summary>
+        /// Returns the number of indeterminate sections of the sequence
+        /// </summary>
+        int AnyBaseCount { get; }
+
+        /// <summary>
+        /// Returns a set of the allowable characters in the sequence for the sequence type
+        /// </summary>
         ISet<char> AllowedCodes { get; }
+
+        /// <summary>
+        /// Returns the metadata tags associated with the sequence
+        /// </summary>
+        ISet<string> Tags { get; }
+        
+        /// <summary>
+        /// Returns a Dictionary of all sequence characters and their counts
+        /// </summary>
         IDictionary<char, long> CodeCounts { get; }
 
         /// <summary>
