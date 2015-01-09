50bc84994314c68380603437ac98db5d95948f42
Rian Stockbower <rstockbower@gmail.com>
2015-01-08 20:59:37 -0500

    Created all the genetic codes with their codon -> protein mappings #15

---
+++ BCompute/Data/GeneticCodes/BacterialArchaealPlantPlastid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BCompute.Data.GeneticCodes
+{
+    public static class BacterialArchaealPlantPlastid
+    {
+        public const int NcbiTranslationTable = 12;
+
+        public static IDictionary<string, AminoAcid> TranscriptionTable
+        {
+            get { return StandardGeneticCode.TranscriptionTable; }
+        } 
+    }
+}
---
+++ BCompute/Data/GeneticCodes/ThraustochytriumMitochondrial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BCompute.Data.GeneticCodes
+{
+    public static class ThraustochytriumMitochondrial
+    {
+        public const int NcbiTranslationTable = 23;
+
+        public static IDictionary<string, AminoAcid> TranscriptionTable
+        {
+            get { return StandardGeneticCode.TranscriptionTable; }
+        }
+    }
+}
