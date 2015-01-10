d9ad5022e91e180d0ca8b3a1321c7190756451bd
Rian Stockbower <rstockbower@gmail.com>
2015-01-10 18:56:58 -0500

    Generic alphabet interface #16

--- BCompute/Interfaces/IAlphabet.cs
+++ BCompute/Interfaces/IAlphabet.cs
@@ -1,7 +1,18 @@
-﻿namespace BCompute.Interfaces
+﻿using System.Collections.Generic;
+using BCompute.Data.GeneticCodes;
+
+namespace BCompute.Interfaces
 {
     interface IAlphabet
     {
+        /// <summary>
+        /// Provides the set of allowed symbols
+        /// </summary>
+        ISet<Nucleotide> AllowedSymbols { get; }
 
+        /// <summary>
+        /// The NCBI genetic code triplet mappings, which governs which transcription table is used for transcribing and back transcribing RNA and DNA
+        /// </summary>
+        GeneticCode GeneticCode { get; }
     }
 }
