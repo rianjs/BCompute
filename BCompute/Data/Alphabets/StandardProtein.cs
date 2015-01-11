2623b196b35c038deb44b912542c03913a33e595
Rian Stockbower <rstockbower@gmail.com>
2015-01-10 22:29:03 -0500

    Renamed enum

--- BCompute/Data/Alphabets/StandardProtein.cs
+++ BCompute/Data/Alphabets/StandardProtein.cs
@@ -4,45 +4,45 @@ using GeneticCode = BCompute.Data.GeneticCodes.GeneticCode;
 
 namespace BCompute.Data.Alphabets
 {
-    internal class StandardProtein : IProteinAlphabet
+    internal class StandardProtein : IProteinSequence
     {
-        private HashSet<Protein> _allowedSymbols;
-        public ISet<Protein> AllowedSymbols
+        private HashSet<ProteinType> _allowedSymbols;
+        public ISet<ProteinType> AllowedSymbols
         {
             get
             {
                 return _allowedSymbols ??
                        (_allowedSymbols =
-                           new HashSet<Protein>
+                           new HashSet<ProteinType>
                            {
-                               Protein.Alanine,
-                               Protein.Cysteine,
-                               Protein.Aspartate,
-                               Protein.Glutamate,
-                               Protein.Phenylalanine,
-                               Protein.Glycine,
-                               Protein.Histidine,
-                               Protein.Isoleucine,
-                               Protein.Lysine,
-                               Protein.Leucine,
-                               Protein.Methionine,
-                               Protein.Asparagine,
-                               Protein.Proline,
-                               Protein.Glutamine,
-                               Protein.Arginine,
-                               Protein.Serine,
-                               Protein.Threonine,
-                               Protein.Valine,
-                               Protein.Tryptophan,
-                               Protein.Tyrosine,
-                               Protein.Stop,
-                               Protein.Gap,
+                               ProteinType.Alanine,
+                               ProteinType.Cysteine,
+                               ProteinType.Aspartate,
+                               ProteinType.Glutamate,
+                               ProteinType.Phenylalanine,
+                               ProteinType.Glycine,
+                               ProteinType.Histidine,
+                               ProteinType.Isoleucine,
+                               ProteinType.Lysine,
+                               ProteinType.Leucine,
+                               ProteinType.Methionine,
+                               ProteinType.Asparagine,
+                               ProteinType.Proline,
+                               ProteinType.Glutamine,
+                               ProteinType.Arginine,
+                               ProteinType.Serine,
+                               ProteinType.Threonine,
+                               ProteinType.Valine,
+                               ProteinType.Tryptophan,
+                               ProteinType.Tyrosine,
+                               ProteinType.Stop,
+                               ProteinType.Gap,
                            });
             }
         }
 
         public GeneticCode GeneticCode { get; private set; }
 
-        public IDictionary<Protein, IEnumerable<string>> ReverseTranscriptionTable { get; private set; }
+        public IDictionary<ProteinType, IEnumerable<string>> ReverseTranscriptionTable { get; private set; }
     }
 }
