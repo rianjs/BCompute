e8fd3a43eaab1b2757c2d9de6f963eee58efc929
Rian Stockbower <rstockbower@gmail.com>
2015-01-11 10:59:00 -0500

    Verified and rearranged nucleotide alphabet interface

--- BCompute/Interfaces/INucleotideAlphabet.cs
+++ BCompute/Interfaces/INucleotideAlphabet.cs
@@ -6,29 +6,29 @@ namespace BCompute.Interfaces
     interface INucleotideAlphabet
     {
         /// <summary>
-        /// The set of symbols that are allowed in the sequence's alphabet
-        /// </summary>
-        ISet<Nucleotide> AllowedSymbols { get; }
-
-        /// <summary>
         /// The NCBI genetic code triplet mappings, which governs which transcription table is used for transcribing and back transcribing RNA and DNA
         /// </summary>
         GeneticCode GeneticCode { get; }
 
         /// <summary>
-        /// Mapping of codon triplets to proteins. (Works for DNA, too.)
+        /// The set of symbols that are allowed in the sequence's alphabet
         /// </summary>
-        IDictionary<Nucleotide, Nucleotide> TranslationTable { get; }
+        ISet<Nucleotide> AllowedSymbols { get; }
 
         /// <summary>
         /// Mapping of nucleotide complements
         /// </summary>
         IDictionary<Nucleotide, Nucleotide> ComplementTable { get; }
-
+        
         /// <summary>
         /// Mapping of DNA to RNA and RNA to DNA transciptions
         /// </summary>
         IDictionary<Nucleotide, Nucleotide> TranscriptionTable { get; }
+
+        /// <summary>
+        /// Mapping of codon triplets to proteins. (Works for DNA, too.)
+        /// </summary>
+        IDictionary<Nucleotide, Nucleotide> TranslationTable { get; }
 
         /// <summary>
         /// Contains the set of symbols used to calculate the GC content of a nucleotide sequence
