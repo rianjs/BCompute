4d7dd26acac46b57e22c132f37d69864761320ca
Rian Stockbower <rstockbower@gmail.com>
2015-01-11 12:10:01 -0500

    AllowedSymbols for NucleotideAlphabet implemented

---
+++ BCompute/Data/Alphabets/NucleotideAlphabet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCompute.Data.GeneticCodes;
+using BCompute.Interfaces;
+
+namespace BCompute.Data.Alphabets
+{
+    internal class NucleotideAlphabet : INucleotideAlphabet
+    {
+        public GeneticCode GeneticCode { get; private set; }
+        public ISet<Nucleotide> AllowedSymbols { get; private set; }
+        public IDictionary<Nucleotide, Nucleotide> ComplementTable { get; private set; }
+        public IDictionary<Nucleotide, Nucleotide> TranscriptionTable { get; private set; }
+        public IDictionary<Nucleotide, Nucleotide> TranslationTable { get; private set; }
+        public ISet<Nucleotide> GcContentSymbols { get; private set; }
+
+        public NucleotideAlphabet(AlphabetType alphabet, GeneticCode geneticCode)
+        {
+            if (alphabet == AlphabetType.ExtendedProtein || alphabet == AlphabetType.StandardProtein)
+            {
+                throw new ArgumentException(String.Format("{0} is not a nucleotide alphabet", alphabet));
+            }
+            GeneticCode = geneticCode;
+            AllowedSymbols = NucleotideAlphabetDataProvider.GetAllowedSymbols(alphabet);
+        }
+    }
+}
---
+++ BCompute/Data/Alphabets/NucleotideAlphabetDataProvider.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCompute.Data.Alphabets
+{
+    internal static class NucleotideAlphabetDataProvider
+    {
+        #region Nucleotide sets
+        private static HashSet<Nucleotide> _allNucleotides;
+        public static ISet<Nucleotide> AllNucleotides
+        {
+            get
+            {
+                if (_allNucleotides == null)
+                {
+                    _allNucleotides = new HashSet<Nucleotide>((Nucleotide[])Enum.GetValues(typeof(Nucleotide)));
+                }
+                return _allNucleotides;
+            }
+        }
+
+        private static HashSet<Nucleotide> _unambiguousDna;
+        public static ISet<Nucleotide> UnambiguousDna
+        {
+            get
+            {
+                if (_unambiguousDna == null)
+                {
+                    _unambiguousDna = new HashSet<Nucleotide>((Nucleotide[])Enum.GetValues(typeof(StrictDna)));
+                }
+                return _unambiguousDna;
+            }
+        }
+
+        public static ISet<Nucleotide> NotInAnyDnaAlphabet = new HashSet<Nucleotide> { Nucleotide.Uracil };
+
+        private static HashSet<Nucleotide> _ambiguousDna;
+        public static ISet<Nucleotide> AmbiguousDna
+        {
+            get
+            {
+                if (_ambiguousDna == null)
+                {
+                    _ambiguousDna = new HashSet<Nucleotide>(AllNucleotides.Except(NotInAnyDnaAlphabet));
+                }
+                return _ambiguousDna;
+            }
+        }
+
+        private static HashSet<Nucleotide> _unambiguousRna;
+        public static ISet<Nucleotide> UnambiguousRna
+        {
+            get
+            {
+                if (_unambiguousRna == null)
+                {
+                    _unambiguousRna = new HashSet<Nucleotide>((Nucleotide[])Enum.GetValues(typeof(StrictRna)));
+                }
+                return _unambiguousRna;
+            }
+        }
+
+        public static ISet<Nucleotide> NotInAnyRnaAlphabet = new HashSet<Nucleotide> { Nucleotide.Thymine };
+
+        private static HashSet<Nucleotide> _ambiguousRna;
+        public static ISet<Nucleotide> AmbiguousRna
+        {
+            get
+            {
+                if (_ambiguousRna == null)
+                {
+                    _ambiguousRna = new HashSet<Nucleotide>(AllNucleotides.Except(NotInAnyRnaAlphabet));
+                }
+                return _ambiguousRna;
+            }
+        }
+        #endregion
+
+        public static ISet<Nucleotide> GetAllowedSymbols(AlphabetType alphabet)
+        {
+            switch (alphabet)
+            {
+                case AlphabetType.UnambiguousDna:
+                    return UnambiguousDna;
+                case AlphabetType.AmbiguousDna:
+                    return AmbiguousDna;
+                case AlphabetType.UnambiguousRna:
+                    return UnambiguousRna;
+                case AlphabetType.AmbiguousRna:
+                    return AmbiguousRna;
+                default:
+                    throw new ArgumentException(String.Format("{0} is not a nucleotide alphabet", alphabet));
+            }
+        }
+    }
+}
