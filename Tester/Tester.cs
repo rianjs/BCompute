73976dcec28d9d8134f845feb4ee1d20dbbd192e
Rian Stockbower <rstockbower@gmail.com>
2015-01-20 20:15:13 -0500

    Misc tester fix

--- Tester/Tester.cs
+++ Tester/Tester.cs
@@ -10,11 +10,17 @@ namespace Tester
         {
             const string gandalfPath = @"M:\dev\rosalind\GuanineCytosineContent\rosalind_gc_orig.txt";
             const string macbookPath = @"C:\Users\rianjs\dev\Rosalind\GuanineCytosineContent\rosalind_gc.txt";
-            const string active = gandalfPath;
-            var parser = new FastaParser(active, AlphabetType.StrictDna);
+            const string active = macbookPath;
+            var parser = new FastaParser(new Uri(active), AlphabetType.StrictDna);
             var results = parser.Parse().ToList();
 
-            Console.WriteLine(results.Count());
+            foreach (var sequence in results)
+            {
+                Console.WriteLine(sequence.Tags.First());
+                Console.WriteLine("{0:N0}", sequence.Sequence.Length);
+            }
+
+            Console.WriteLine(results.Count);
             Console.ReadLine();
         }
     }
