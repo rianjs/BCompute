e484847659e9d86b538e3a29b35c88347e8abc1b
Rian Stockbower <rstockbower@gmail.com>
2015-01-24 18:12:14 -0500

    Sexual reproduction modeling completed with unit tests. #58

---
+++ BCompute.UnitTests/SexualReproductionUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BCompute.UnitTests
+{
+    public class SexualReproductionUnitTests
+    {
+        [Test, TestCaseSource("GetPopulationCount_TestCases")]
+        public long GetPopulationCount_Tests(int initialPopulation, int offspring, int lifespan, int generations)
+        {
+            var model = new SexualReproductionModeler(initialPopulation, offspring, lifespan);
+            return model.GetPopulationCount(generations);
+        }
+
+        public IEnumerable<ITestCaseData> GetPopulationCount_TestCases()
+        {
+            yield return new TestCaseData(1, 3, int.MaxValue, 5).Returns(19).SetName("Immortal rabbits, small number of generations");
+            yield return new TestCaseData(1, 1, 3, 6).Returns(4).SetName("Mortal rabbits, small number of generations");
+            yield return new TestCaseData(1, 3, int.MaxValue, 35).Returns(1323839213083).SetName("Immortal rabbits, large number of generations");
+            yield return new TestCaseData(1, 1, 18, 87).Returns(676172117379838466).SetName("Mortal rabbits, large number of generations");
+
+            yield return new TestCaseData(-1, 1, 18, 87).Throws(typeof(ArgumentException)).SetName("Negative initial population throws exception");
+            yield return new TestCaseData(1, -1, 18, 87).Throws(typeof(ArgumentException)).SetName("Negative offspring per generation throws exception");
+            yield return new TestCaseData(1, 1, -18, 87).Throws(typeof(ArgumentException)).SetName("Negative lifespan throws exception");
+            yield return new TestCaseData(100, 1, 18, 1).Returns(100).SetName("Population count of first generation is the initial population");
+        }
+    }
+}
--- Tester/Tester.cs
+++ Tester/Tester.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using BCompute;
@@ -11,14 +12,27 @@ namespace Tester
     {
         static void Main()
         {
-            var generations = 6;
-            var growthPerGeneration = 2;
-            var survivalGenerations = int.MaxValue;
+            var initialPopulation = 1;
+            var maturity = 1;
+            var offspringPerCycle = 1;
+            var lifespan = 3;
+            //var population = new SexualReproductionModeler(initialPopulation, maturity, offspringPerCycle, lifespan);
+            //var foo = population.GetPopulationCount(6);     //Should be 4
 
-            var foo = ReprodutiveModeling.Growth(generations, growthPerGeneration, survivalGenerations);
-            Console.WriteLine(foo);
+            //var immortal = new SexualReproductionModeler(1, 1, 3, int.MaxValue);
+            //var immortalAnswer = immortal.GetPopulationCount(5);
+            //Console.WriteLine(foo);
+            //Console.WriteLine(immortalAnswer);  //Should be 19
 
+            var bigTest = new SexualReproductionModeler(initialPopulation, offspringPerCycle, int.MaxValue);
+            var immortalTotal = bigTest.GetPopulationCount(35);
+            Console.WriteLine("{0:N0}", immortalTotal);
 
+            lifespan = 18;
+            var actualTest = new SexualReproductionModeler(initialPopulation, maturity, offspringPerCycle, lifespan);
+            var total = actualTest.GetPopulationCount(87);      //Should be 676172117379838466 -- OK
+            Console.WriteLine(total);
+            Console.WriteLine("{0:N0}", total);
             Console.ReadLine();
         }
     }
