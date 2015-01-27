0172967c73b043a0136873a0dc53b48b80997ae1
Rian Stockbower <rstockbower@gmail.com>
2015-01-27 15:49:24 -0500

    Sexual reproduction model constructor tests extracted into their own method pairs #70

--- BCompute.UnitTests/Population/SexualReproductionUnitTests.cs
+++ BCompute.UnitTests/Population/SexualReproductionUnitTests.cs
@@ -6,6 +6,20 @@ namespace BCompute.UnitTests
 {
     public class SexualReproductionUnitTests
     {
+        [Test, TestCaseSource("Constructor_TestCases")]
+        public int Constructor_Tests(int initialPopulation, int offspring, int lifespan, int generations)
+        {
+            var model = new SexualPopulationGrowthModeler(initialPopulation, offspring, lifespan);
+            return 0;
+        }
+
+        public IEnumerable<ITestCaseData> Constructor_TestCases()
+        {
+            yield return new TestCaseData(-1, 1, 18, 87).Throws(typeof(ArgumentException)).SetName("Negative initial population throws exception");
+            yield return new TestCaseData(1, -1, 18, 87).Throws(typeof(ArgumentException)).SetName("Negative offspring per generation throws exception");
+            yield return new TestCaseData(1, 1, -18, 87).Throws(typeof(ArgumentException)).SetName("Negative lifespan throws exception");
+        }
+        
         [Test, TestCaseSource("GetPopulationCount_TestCases")]
         public long GetPopulationCount_Tests(int initialPopulation, int offspring, int lifespan, int generations)
         {
@@ -19,10 +33,6 @@ namespace BCompute.UnitTests
             yield return new TestCaseData(1, 1, 3, 6).Returns(4).SetName("Mortal rabbits, small number of generations");
             yield return new TestCaseData(1, 3, int.MaxValue, 35).Returns(1323839213083).SetName("Immortal rabbits, large number of generations");
             yield return new TestCaseData(1, 1, 18, 87).Returns(676172117379838466).SetName("Mortal rabbits, large number of generations");
-
-            yield return new TestCaseData(-1, 1, 18, 87).Throws(typeof(ArgumentException)).SetName("Negative initial population throws exception");
-            yield return new TestCaseData(1, -1, 18, 87).Throws(typeof(ArgumentException)).SetName("Negative offspring per generation throws exception");
-            yield return new TestCaseData(1, 1, -18, 87).Throws(typeof(ArgumentException)).SetName("Negative lifespan throws exception");
             yield return new TestCaseData(100, 1, 18, 1).Returns(100).SetName("Population count of first generation is the initial population");
         }
     }
